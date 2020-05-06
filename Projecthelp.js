68d85db22fbf6c5885f28454b2bf53af11c6af43
TheBrilliantProgrammer <63143832+TheBrilliantProgrammer@users.noreply.github.com>
2020-05-06 14:47:39 +0400

    Create Projecthelp.js

---
+++ Projecthelp.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+require("dotenv").config();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "Project 0") {
+    msg.reply("#project-0 Project Homepage link:https://docs.cs50.net/web/2020/x/projects/0/project0.html");
+  }
+});
+
+client.login(process.env.TOKEN);
